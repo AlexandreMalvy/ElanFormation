23977bb6c150c3b68112cc349368f33ed619475e
AlexandreMalvy <damdam082612@gmail.com>
2021-05-25 16:46:09 +0200

    Ajout d'un formulaire

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Module;
 use App\Entity\Centre;
+use App\Entity\Module;
+use App\Form\CentreType;
 use App\Entity\Formation;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,6 +15,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class ActivityController extends AbstractController
 {
+    /**
+     * @Route("/addcentre", name="centre_add")
+     * @Route("/{id}/editcentre", name="centre_edit")
+     */
+    public function add_edit_centre(Centre $centre = null, Request $request){
+        // si le centre n'existe pas, on instancie un nouveau centre(on est dans le cas d'un ajout) 
+        if(!$centre){
+            $centre = new Centre();
+        }
+        //il faut créer un SalarieType au préalable (php bin/console make:form)
+        $form = $this->createForm(CentreType::class, $centre );
+
+        $form->handleRequest($request);
+        // si on soumet le formulaire et que le form est validé
+        if($form->isSubmitted() && $form->isValid()){
+            //on récuprère les données du formulaire
+            $centre = $form->getData();
+            //on ajoute le nouveau centre
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($centre);
+            $entityManager->flush();
+            //on redirige vers la liste des centre (centre_list etant le nom de la route)
+            return $this->redirectToRoute("activitys_index");
+
+        }
+        return $this->render('activity/add_edit_centre.html.twig', [
+            'formCentre' => $form->createView(),
+            'editMode'=> $centre->getId() !== null
+        ]);
+    }
     /**
      * @Route("/", name="activitys_index")
      */
---
+++ src/Form/CentreType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Centre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CentreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ville', TextType::class, [])
+            ->add('adresse', TextType::class, [])
+            ->add('cp', TextType::class, [])
+            ->add('Valider', SubmitType::class, [])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Centre::class,
+        ]);
+    }
+}
