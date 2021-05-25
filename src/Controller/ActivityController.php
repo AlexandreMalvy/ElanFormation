9e1f28670d662f73d1f403efcb7eddc8211d0ab9
AlexandreMalvy <damdam082612@gmail.com>
2021-05-25 15:11:17 +0200

    avancement des controller

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,19 +2,51 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Modul;
+use App\Entity\Centre;
+use App\Entity\Formation;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+/**
+ * @Route("/activitys")
+ */
 class ActivityController extends AbstractController
 {
     /**
-     * @Route("/activity", name="activity")
+     * @Route("/", name="activitys_index")
      */
-    public function index(): Response
+    public function index()
     {
+        $centres = $this->getDoctrine()
+                ->getRepository(Centre::class);
+
+        $formations = $this->getDoctrine()
+                ->getRepository(Formation::class);
+
+        $modules = $this->getDoctrine()
+                ->getRepository(Modul::class);
+
         return $this->render('activity/index.html.twig', [
-            'controller_name' => 'ActivityController',
+            'centres' => $centres,
+            "formations" => $formations,
+            "modules" => $modules
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="centre_show", methods="GET")
+     */
+    public function showCentre(Centre $centre): Response {
+        return $this->render('activity/showCentre.html.twig', [
+            'centre' => $centre
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="formation_show", methods="GET")
+     */
+    public function showFormation(Formation $formation): Response {
+        return $this->render('activity/showFormation.html.twig', [
+            'formation' => $formation
         ]);
     }
 }
