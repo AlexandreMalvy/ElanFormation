8234b29679a8d37e5a537bf9b713bbeadade9f45
AlexandreMalvy <damdam082612@gmail.com>
2021-05-26 09:43:34 +0200

    Ajout du formulaire de formation

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,6 +6,7 @@ use App\Entity\Centre;
 use App\Entity\Module;
 use App\Form\CentreType;
 use App\Entity\Formation;
+use App\Form\FormationType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -46,15 +47,47 @@ class ActivityController extends AbstractController
         ]);
     }
     /**
+     * @Route("/addformation", name="formation_add")
+     * @Route("/{id}/editformation", name="formation_edit")
+     */
+    public function add_edit_formation(Formation $formation = null, Request $request){
+        // si le formation n'existe pas, on instancie un nouveau formation(on est dans le cas d'un ajout) 
+        if(!$formation){
+            $formation = new Formation();
+        }
+        //il faut créer un SalarieType au préalable (php bin/console make:form)
+        $form = $this->createForm(FormationType::class, $formation );
+
+        $form->handleRequest($request);
+        // si on soumet le formulaire et que le form est validé
+        if($form->isSubmitted() && $form->isValid()){
+            //on récuprère les données du formulaire
+            $formation = $form->getData();
+            //on ajoute le nouveau formation
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($formation);
+            $entityManager->flush();
+            //on redirige vers la liste des formation (formation_list etant le nom de la route)
+            return $this->redirectToRoute("activitys_index");
+
+        }
+        return $this->render('activity/add_edit_formation.html.twig', [
+            'formFormation' => $form->createView(),
+            'editMode'=> $formation->getId() !== null
+        ]);
+    }
+    /**
      * @Route("/", name="activitys_index")
      */
     public function index()
     {
         $centres = $this->getDoctrine()
-                ->getRepository(Centre::class);
+                ->getRepository(Centre::class)
+                ->findAll();
 
         $formations = $this->getDoctrine()
-                ->getRepository(Formation::class);
+                ->getRepository(Formation::class)
+                ->findAll();
 
         $modules = $this->getDoctrine()
                 ->getRepository(Module::class);
