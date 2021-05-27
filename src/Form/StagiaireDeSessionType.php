dd1efdcf5fdcaad7d6083dc6fbf0fcc6fa449d49
Tablieh <waledo.1997@hotmail.com>
2021-05-27 09:43:30 +0200

    new StagiairedesessionType

--- src/Form/StagiaireDeSessionType.php
+++ src/Form/StagiaireDeSessionType.php
@@ -6,12 +6,8 @@ use App\Entity\Stagiaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class StagiaireType extends AbstractType
+class StagiaireDeSessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -24,7 +20,11 @@ class StagiaireType extends AbstractType
             ->add('cp', NumberType::class, [])
             ->add('ville', TextType::class, [])
             ->add('telephone', NumberType::class, [])
-            ->add('Valider', SubmitType::class, [])
+            ->add('sessions', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'titre',
+                'attr' => ["class" => "uk-select"]
+            ]);
         ;
     }
 
